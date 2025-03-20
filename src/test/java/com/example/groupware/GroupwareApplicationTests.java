6310ea44883e50e635be5819b1bca852a3d7e0fe
백승우 <tmddn7439@naver.com>
2025-03-20 22:57:42 +0900

    Initial commit

---
+++ src/test/java/com/example/groupware/GroupwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groupware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
